120d16b831c17c6aee5beb0bab5b65e09cdddc5a
Aayush Shrestha <aayushstha20@gmail.com>
2025-07-23 18:11:40 +0545

    Fixed minor bugs for deployment issue

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import TextForm from './TextForm'
 
 export default function NavBar({ headText = "HeadText Here", aboutText = "AboutText Here", changeStyle }) {
     return (
