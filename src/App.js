b3db2f7eaba224afd26ab284ace8fc743d762fd8
Aayush Shrestha <aayushstha20@gmail.com>
2025-07-23 13:33:54 +0545

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
       <>
-        
+        {/* <NavBar/> */}
+        <NavBar headText="TextUtils" aboutText="About TextUtils"/>
       </>
   );
 }
